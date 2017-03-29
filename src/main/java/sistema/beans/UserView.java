5350bb6236ba05b42e13c17547fd55fbff86f66f
Juan Cardoso <juancardoso@outlook.com>
2017-03-29 15:14:13 -0300

    ALTERAÇÕES NA TELA DE "NÃO TENHO CADASTRO" OLHAR LÁ A OBSERVAÇÕES

---
+++ src/main/java/sistema/beans/UserView.java
@@ -0,0 +1,69 @@
+package sistema.beans;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+ 
+@ManagedBean
+public class UserView {
+     
+    private String nome;
+    private String usuario;
+    private String senha;
+    private String rg;
+    private String cpf;
+    private String tipo;
+    
+    public void save() {
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage("Welcome " + nome + " " + usuario));
+    }
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+}
